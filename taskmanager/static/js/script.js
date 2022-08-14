c9e8562ac51848525dd8b8281ca89abdf18c93e7
ejasquith <emily.asquith@icloud.com>
2022-08-14 02:39:27 +0000

    Implement create task functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -3,4 +3,12 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
     // Copyright year initialisation
     $('#copyright').text(new Date().getFullYear());
+    // datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+    })
+    // select init
+    $('select').formSelect();
   });
